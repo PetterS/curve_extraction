910462dca3d40a3e7f78566067eb1e5c6254cb3e
Johannes Ulén <johannes.ulen@gmail.com>
2014-01-10 20:01:34 +0100

    Return 2D curve from lokal optimization of 2D curves

--- matlab/library/local_optimization.m
+++ matlab/library/local_optimization.m
@@ -1,6 +1,8 @@
 % Wrapper for mex function.
 function [optimized_path, solution_cost, solve_time] = local_optimization(mesh_map, unary, input_path, settings)
 
+problem_size = size(unary);
+
 % Check file modification dates and recompile mex file
 my_name = mfilename('fullpath');
 [base_path, base_name, ~] = fileparts(my_name);
@@ -11,7 +13,7 @@ compile(base_path, base_name)
 % Checks for disallowed pixels
 unary(mesh_map == 0) = inf;
 
-if size(input_path, 2) == 2
+ if length(problem_size) == 2
    input_path = [input_path ones(size(input_path,1),1)]; 
 end
 
@@ -19,3 +21,8 @@ end
 
 % Check that the solution fulfills constraints
 assert( size(optimized_path,2) == size(input_path, 2) );
+
+% Remove padding
+if length(problem_size) == 2
+	optimized_path	= optimized_path(:,1:2);
+end
