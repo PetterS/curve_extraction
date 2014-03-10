352977b91dc65c2b9e3561e43f760085ebb812f4
Petter Strandmark <petter.strandmark@gmail.com>
2014-03-10 23:31:15 +0100

    Clearer (???) difference with and without constraints in example.

--- matlab/constrained_optimization.m
+++ matlab/constrained_optimization.m
@@ -1,7 +1,7 @@
 close all; clear all;
 rng(0)
 n = 100;
-data = rand(n,n);
+data = 100*rand(n,n);
 
 start_set = false(size(data));
 end_set = false(size(data));
@@ -17,8 +17,14 @@ C.set_connectivity_by_radius(3);
 C.length_penalty = 0.25;
 C.curvature_penalty = 0.25;
 
+figure(1);
+
+C.shortest_path();
+C.plot_curve()
+
+figure(2);
 % Upper bounds on the length and curvature.
-C.length_limit = 120;
+C.length_limit = 115;
 C.curvature_limit = 2;
 
 C.shortest_path();
